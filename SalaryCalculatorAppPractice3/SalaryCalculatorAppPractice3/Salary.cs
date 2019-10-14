754204d26a9229d6bc1406415ceba7de1f3204f4
ARMilon927 <a.r.milon927@gmail.com>
2019-10-15 01:16:05 +0600

    Practice is done

---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/Salary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class Salary
+    {
+        public string employeeName;
+        public double basicAmount;
+        public double totalSalary;
+
+        public double GetTotalSalary(double rent, double medical)
+        {
+            totalSalary = basicAmount + basicAmount * (rent + medical) / 100;
+            return totalSalary;
+        }
+    }
+}
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+        Salary aSalary = new Salary();
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            aSalary.employeeName = employeeNameTextBox.Text;
+            aSalary.basicAmount = Convert.ToDouble(basicAmountTextBox.Text);
+            double rent = Convert.ToDouble(homeRentTextBox.Text);
+            double medical = Convert.ToDouble(medicalAllowanceTextBox.Text);
+            double totalSalary = aSalary.GetTotalSalary(rent, medical);
+            MessageBox.Show(aSalary.employeeName + " , your salary is : " + totalSalary);
+        }
+    }
+}
